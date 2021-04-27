ca302568b116f25a1c4b8efa9c5d6d1ebd70bde1
Ariya Adinatha <13519048@std.stei.itb.ac.id>
2021-04-27 23:05:29 +0700

    chat diperbaharui menggunakan append

--- src/app.py
+++ src/app.py
@@ -4,20 +4,13 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        time = datetime.now()
-        currentTime = time.strftime("%H:%M")
-        chat = request.form['user-chat']
-        arrayChatUser.append([chat, currentTime])
-        return redirect('/')
-    else:
-        botTime = datetime.now()
-        currentBotTime = botTime.strftime("%H:%M")
-        return render_template('index.html', arrayChatUser = arrayChatUser, currentBotTime = currentBotTime)
+    return render_template('index.html')
+
+@app.route("/bot")
+def getBotResponse():
+    return 'Hello world'
 
 if __name__ == "__main__":
-    arrayChatUser = []
-    arrayChatBot = []
     app.run(debug=True)
