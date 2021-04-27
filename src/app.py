4800fe0f51004b7282cb2c05ab3d0b73f687f03f
Ariya Adinatha <13519048@std.stei.itb.ac.id>
2021-04-27 17:41:44 +0700

    ui beres

--- src/app.py
+++ src/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import datetime
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
-
+    if request.method == 'POST':
+        time = datetime.now()
+        currentTime = time.strftime("%H:%M")
+        chat = request.form['user-chat']
+        arrayChatUser.append([chat, currentTime])
+        return redirect('/')
+    else:
+        botTime = datetime.now()
+        currentBotTime = botTime.strftime("%H:%M")
+        return render_template('index.html', arrayChatUser = arrayChatUser, currentBotTime = currentBotTime)
 
 if __name__ == "__main__":
+    arrayChatUser = []
+    arrayChatBot = []
     app.run(debug=True)
---
+++ src/test.py
@@ -0,0 +1,4 @@
+arrayChat = []
+arrayChat.append(['asd',123])
+for chat in arrayChat:
+    print(chat[0])
