4ef15618fbb4ebf0a2267c78b13579c99eb9eced
jonathanrichard13 <64012903+jonathanrichard13@users.noreply.github.com>
2021-04-28 13:16:32 +0700

    handleInput beres

--- src/handleInput.py
+++ src/handleInput.py
@@ -10,13 +10,13 @@ def getListOfKataPenting():
 
 def getFlag():
     # TODO : Lengkapin Flag
-    return ["help", "assistant", "diundur", "selesai", "deadline", "apa"]
+    return ["help", "assistant", "diundur", "selesai", "kapan", "deadline", "apa saja", "apa aja"]
 
 
 def GetInputDate(inputTask):
 
     inputDate = re.findall(
-        "(\d{1,2}).(\d{1,2}).(\d{4})|(\d{1,2}).(\w+).(\d{4})", inputTask)
+        "(\d{1,2}).(\d{1,2}).(\d{2,4})|(\d{1,2}).(\w+).(\d{4})", inputTask)
     filteredDate = []
     for date in inputDate:
         filteredData = []
@@ -36,6 +36,8 @@ def DateConverter(inputListDate):
 
         intMonth = int(inputListDate[1])
 
+        if(len(inputListDate[2])==2):
+            inputListDate[2] = "20" + inputListDate[2]
         return datetime.datetime(int(inputListDate[2]), intMonth, int(inputListDate[0])).strftime("%d/%m/%Y")
     except:
         pass
@@ -46,7 +48,7 @@ def DateConverter(inputListDate):
             break
         i += 1
     if i == 12:
-        raise Exception("Invalid Month Input!")
+        raise Exception("Invalid Date Input!")
     return datetime.datetime(int(inputListDate[2]), i+1, int(inputListDate[0])).strftime("%d/%m/%Y")
 
 
@@ -65,13 +67,16 @@ def GetInputKodeKuliah(inputTask):
 
 def GetInputDeskripsiTugas(inputTask):
     task = re.search(
-        "([a-zA-Z][a-zA-Z]\d{4})(.(\w*))*(?=\s(\d{1,2}).(\d{1,2}).(\d{4})|\s(\d{1,2}).(\w+).(\d{4}))", inputTask)
+        "([a-zA-Z][a-zA-Z]\d{4})(.(\w*))*(?=\s(\d{1,2}).(\d{1,2}).(\d{2,4})|\s(\d{1,2}).(\w+).(\d{4}))", inputTask)
+    print("Task: " + str(task.group()))
     if task is None:
-        return ""
+        return "No description provided"
     task = task.group().strip(" pada")
-    inputDeskripsiTugas = re.search(
-        "(?<=([a-zA-Z][a-zA-Z]\d{4}\s))(.(\w*))*", task).group()
-    return inputDeskripsiTugas
+    temp = re.search(
+        "(?<=([a-zA-Z][a-zA-Z]\d{4}\s))(.(\w*))*", task)
+    if (temp is None):
+        return "No description provided"
+    return temp.group()
 
 
 def GetKategoriTugas(inputTask):
@@ -79,7 +84,7 @@ def GetKategoriTugas(inputTask):
     idx = -1
 
     for i in range(len(getListOfKataPenting())):
-        if (boyer_moore.boyer_moore_string_matching(inputTask, getListOfKataPenting()[i])):
+        if (boyer_moore.boyer_moore_string_matching(inputTask.lower(), getListOfKataPenting()[i].lower())):
             count += 1
             idx = i
     if count > 1:
@@ -94,20 +99,24 @@ def query(database, queries, idx):
     result = []
     for query in queries:
         for data in database:
-            if data[idx] == query:
+            if data[idx].lower() == query.lower():
                 result.append(data)
     return result
 
 
-def task_viewer(sentence, indeks):
+def task_viewer(sentence, indeks, database):
     database = fileIO.loadTask()
-
-    kata_penting = getListOfKataPenting()
+    if (indeks == 2):
+        kata_penting = getListOfKataPenting()
+    elif (indeks == 1):
+        kata_penting = ["Tubes", "Tucil"]
     match = []
+    print("sentence: "+ str(sentence))
     for kata in kata_penting:
-        if (boyer_moore.boyer_moore_string_matching(sentence, kata)):
+        print("Kata: " + str(kata))
+        if (boyer_moore.boyer_moore_string_matching(sentence.lower(), kata.lower())):
             match += [kata]
-
+    print("Match awal: " + str(match))
     print(database)
     print("match: " + str(match))
     db = []
@@ -121,7 +130,10 @@ def task_viewer(sentence, indeks):
     print(db)
     # Dari regex
     list_of_date = GetInputDate(sentence)
-    list_of_date = [DateConverter(inputDate) for inputDate in list_of_date]
+    try :
+        list_of_date = [DateConverter(inputDate) for inputDate in list_of_date]
+    except:
+        return ["Invalid Date Input!"]
 
     # for i in range(len(list_of_date)):
     #     list_of_date[i]
@@ -143,7 +155,7 @@ def task_viewer(sentence, indeks):
         idIDX = re.findall("(?<=\s)(\d+)(?=\s)", sentence)
         time_unit_found = []
         for unit in time_unit:
-            if (boyer_moore.boyer_moore_string_matching(sentence, unit)):
+            if (boyer_moore.boyer_moore_string_matching(sentence.lower(), unit.lower())):
                 time_unit_found.append(unit)
 
         if (idIDX == []):
@@ -229,16 +241,22 @@ def task_viewer(sentence, indeks):
 
     print("Final: " + str(final))
     if (indeks == 1):
-        return query(final, ["Tubes", "Tucil"], 2)
+        print("DIsini")
+        # print("Match akhir: " + str)
+        if (len(match) == 0):
+            return query(final, ["Tubes", "Tucil"], 2)
+        return query(final, match, 2)
     # tugas_in_match = [match[i] for i in range(
     #     len(match)) if match[i] in ["Tubes", "Tucil"]]
     # if (tugas_in_match == []):
     #     print("finals: " + str(final))
     else:
+        print("Match: " + str(match))
         if (match == []):
             print("finals2: " + str(final))
             return final
         elif (len(match) == 1):
+            print("MASUK")
             return query(final, match, 2)
         else:
             return ["Error"]
@@ -254,11 +272,14 @@ def HandleInput(inputTask):
     except Exception:
         pass
 
+    print("listOfTask: " + str(ListOfTask))
+
     flagFound = ""
     for flag in getFlag():
         if boyer_moore.boyer_moore_string_matching(inputTask.lower(), flag):
             flagFound = (flag)
             break
+
     if len(flagFound) == 0:
 
         inputDate = GetInputDate(inputTask)
@@ -272,65 +293,105 @@ def HandleInput(inputTask):
         if len(inputDate) > 1:
             return "Invalid Input! There can only be one date in input!"
         else:
-
-            if inputDate and inputKodeKuliah and inputKategoriTugas and inputDeskripsiTugas:
+            print("START")
+            print(inputDate)
+            print(inputDeskripsiTugas)
+            print(inputKategoriTugas)
+            print(inputKodeKuliah)
+            print("END")
+            try :
+                inputDate[0] = DateConverter(inputDate[0])
+            except:
+                return "Invalid Date Input!"
+            if inputDate and inputKodeKuliah and inputKategoriTugas :
                 # print("Befero Append")
-                ListOfTask.append([DateConverter(
-                    inputDate[0]), inputKodeKuliah, inputKategoriTugas, inputDeskripsiTugas, False])
+                ListOfTask.append([inputDate[0], inputKodeKuliah, inputKategoriTugas, inputDeskripsiTugas, False])
                 # print("after Append")
                 fileIO.saveTask(ListOfTask)
 
-                output = "[TASK BERHASIL DICATAT]\n"
+                output = "[TASK BERHASIL DICATAT]\n<br>"
                 output += "(ID: "+str(len(ListOfTask))+") "+str(ListOfTask[-1][0])+" - " + str(
-                    ListOfTask[-1][1])+" - "+str(ListOfTask[-1][2]) + " - "+str(ListOfTask[-1][3])+"\n"
+                    ListOfTask[-1][1])+" - "+str(ListOfTask[-1][2]) + " - "+str(ListOfTask[-1][3])+"\n<br>"
                 return output
             else:
                 return "Invalid Input! No Argument Detected"
     elif("help" in flag) or ("assistant" in flag):
         return """
-            [Fitur]
-            1. Menambahkan task baru
-            2. Melihat daftar task
-            3. Melihat deadline dari suatu task
-            4. Memperbaharui task
-            5. Menandai suatu task sudah selesai
-            6. Menampilkan opsi help
-
-            [Daftar kata penting]
-            1. Kuis
-            2. Ujian
-            3. Tucil
-            4. Tubes
-            5. Praktikum
-
+            [Fitur]<br>
+            1. Menambahkan task baru<br>
+            2. Melihat daftar task<br>
+            3. Melihat deadline dari suatu task<br>
+            4. Memperbaharui task<br>
+            5. Menandai suatu task sudah selesai<br>
+            6. Menampilkan opsi help<br>
+            <br>
+            [Daftar kata penting]<br>
+            1. Kuis<br>
+            2. Ujian<br>
+            3. Tucil<br>
+            4. Tubes<br>
+            5. Praktikum<br>
         """
     elif ("deadline" in flag):
-        result = task_viewer(inputTask, 1)
+        print("listOfTask: " + str(ListOfTask))
+        result = task_viewer(inputTask, 1, ListOfTask)
+        print(str(result))
         if (result == []):
             return "Tidak ada"
         elif (result == ["Error"]):
             return "Error"
+        elif (result == ["Invalid Date Input!"]):
+            return "Invalid Date Input!"
         else:
-            output = "[Daftar Deadline]\n"
+            print(str(result))
+            output = "[Daftar Deadline]\n<br><br>"
             idx = 1
             for each in result:
                 output += str(idx) + ". (ID: " + str(each[5]) + ") " + each[0] + \
-                    " - " + each[1] + " - " + each[2] + " - " + each[3] + "\n"
+                    " - " + each[1] + " - " + each[2] + " - " + each[3] + "\n<br>"
+                idx += 1
             return output
 
-    elif ("ada" in flag):
-        result = task_viewer(inputTask, 2)
+    elif ("apa saja" in flag) or ("apa aja" in flag):
+        print("listOfTask: " + str(ListOfTask))
+        result = task_viewer(inputTask, 2, ListOfTask)
+        print("Result: " + str(result))
         if (result == []):
             return "Tidak ada"
         elif (result == ["Error"]):
             return "Error"
+        elif (result == ["Invalid Date Input!"]):
+            return "Invalid Date Input!"
         else:
-            output = "[Daftar Deadline]\n"
+            output = "[Daftar Task]\n<br><br>"
             idx = 1
             for each in result:
                 output += str(idx) + ". (ID: " + str(each[5]) + ") " + each[0] + \
-                    " - " + each[1] + " - " + each[2] + " - " + each[3] + "\n"
+                    " - " + each[1] + " - " + each[2] + " - " + each[3] + "\n<br>"
+                idx += 1
             return output
+    
+    elif ("kapan" in flag):
+        print("listOfTask: " + str(ListOfTask))
+        result = task_viewer(inputTask, 2, ListOfTask)
+        print("Result: " + str(result))
+        if (result == []):
+            return "Tidak ada"
+        elif (result == ["Error"]):
+            return "Error"
+        elif (result == ["Invalid Date Input!"]):
+            return "Invalid Date Input!"
+        else:
+            if (len(result) == 1):
+                return result[0][0]
+            else :
+                output = ""
+                idx = 1
+                for each in result:
+                    output += str(idx) + ". (ID: " + str(each[5]) + ") " + each[0] + \
+                        " - " + each[1] + " - " + each[2] + " - " + each[3] + "\n<br>"
+                    idx+=1
+                return output
 
     elif ("diundur" in flag):
         idIDXList = re.findall("(\w+)\s(\d+)", inputTask)
@@ -339,7 +400,10 @@ def HandleInput(inputTask):
         print(idIDX)
         idIDX = [id[1] for id in idIDX]
         print(idIDX)
-        tgl_diundur = DateConverter(GetInputDate(inputTask)[0])
+        try:
+            tgl_diundur = DateConverter(GetInputDate(inputTask)[0])
+        except:
+            return "Invalid Date Input!"
         found = False
         for i in range(len(ListOfTask)):
             if str(i+1) in (idIDX):
@@ -352,7 +416,9 @@ def HandleInput(inputTask):
     elif ("selesai" in flag):
         idIDXList = re.findall("(\w+)\s(\d+)", inputTask)
         print(idIDXList)
-        idIDX = [id for id in idIDXList if "task" in id]
+        idIDX = [id for id in idIDXList if "task" in id[0].lower()]
+        print(idIDX)
+        idIDX = [id[1] for id in idIDX]
         print(idIDX)
         found = False
         for i in range(len(ListOfTask)):
@@ -369,11 +435,11 @@ def HandleInput(inputTask):
 
 
 if __name__ == "__main__":
-    print(task_viewer("Apa aja Praktikum hari ini", 2))
+    print(HandleInput("kapan Praktikum?"))
     # print(HandleInput(" Deadline task 1 dan TASK 3 diundur menjadi 28 april 2021 "))
     # print(HandleInput("deadline tugas 5 sudah selesai"))
     # print(HandleInput("Tubes IF2211 String Matching pada 14-02-2021"))
 
     # for j in range(len(ListOfTask)):
-    #                         output+=str(j+1)+" "+ListOfTask[j][0]+" - "+ListOfTask[j][1]+" - "+ListOfTask[j][2]+" - "+ListOfTask[j][3]+"\n"
+    #                         output+=str(j+1)+" "+ListOfTask[j][0]+" - "+ListOfTask[j][1]+" - "+ListOfTask[j][2]+" - "+ListOfTask[j][3]+"\n<br>"
     #                     return output
